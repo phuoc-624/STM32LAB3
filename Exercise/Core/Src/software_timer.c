96fed77ca4d55c3d63bab2b46eb534771102a20b
DESKTOP-0NSG2HJ\Admin <phuoc.nguyen622004@hcmut.edu.vn>
2025-10-16 02:19:43 +0700

    Init STM32 project

---
+++ Exercise/Core/Src/software_timer.c
@@ -0,0 +1,75 @@
+/*
+ * software_timer.c
+ *
+ *  Created on: Sep 29, 2025
+ *      Author: Admin
+ */
+
+#include "software_timer.h"
+
+//int TIMER_CYCLE = (htim2.Init.Prescaler + 1) * (htim2.Init.Period + 1) / 8000000;
+int TIMER_CYCLE = 10;
+
+int timer1_counter = 0;
+int timer1_flag = 0;
+
+int timer2_counter = 0;
+int timer2_flag = 0;
+
+int timer3_counter = 0;
+int timer3_flag = 0;
+
+int timer4_counter = 0;
+int timer4_flag = 0;
+
+void setTimer1(int duration)
+{
+	timer1_counter = duration / TIMER_CYCLE;
+	timer1_flag = 0;
+}
+
+void setTimer2(int duration)
+{
+	timer2_counter = duration / TIMER_CYCLE;
+	timer2_flag = 0;
+}
+
+void setTimer3(int duration)
+{
+	timer3_counter = duration / TIMER_CYCLE;
+	timer3_flag = 0;
+}
+
+void setTimer4(int duration)
+{
+	timer4_counter = duration / TIMER_CYCLE;
+	timer4_flag = 0;
+}
+
+void timerRun()
+{
+	if(timer1_counter > 0)
+	{
+		timer1_counter--;
+		if(timer1_counter <= 0)
+		timer1_flag = 1;
+	}
+	if(timer2_counter > 0)
+	{
+		timer2_counter--;
+		if(timer2_counter <= 0)
+		timer2_flag = 1;
+	}
+	if(timer3_counter > 0)
+	{
+		timer3_counter--;
+		if(timer3_counter <= 0)
+		timer3_flag = 1;
+	}
+	if(timer4_counter > 0)
+	{
+		timer4_counter--;
+		if(timer4_counter <= 0)
+		timer4_flag = 1;
+	}
+}
